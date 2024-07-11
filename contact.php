0f0c74c810945c8d96775ff343329020cc9de71b
screpinweb <sylviecrepin.web@gmail.com>
2024-07-11 15:01:05 -0400

    Contact Location Adjustment

--- contact.php
+++ contact.php
@@ -93,14 +93,14 @@
                 </div>
 
                 <div class="row vr-size-1 clinic-info">
-                  <div class="col-lg-3">
+                  <div class="col-lg-4">
                     <h6>Clinique Santé Optimum</h6>
                     <p class="clinic-info">
                         1500 Montgolfier Suite 400<br> Laval, QC, H7T 0A2<br>
                         (450) 497-1500 x3
                     </p>
                   </div>
-                  <div class="col-lg-3">
+                  <div class="col-lg-4">
                     <h6>SM PRIVÉ - Clinique Médicale</h6>
                     <p>
                         3333 Blvd Graham #220 <br>Mont Royal, Québec H3R 3L5<br>
@@ -108,7 +108,7 @@
                         +1 (833) 921-2783
                     </p>
                   </div>
-                  <div class="col-lg-3">
+                  <div class="col-lg-4">
                     <h6>SM PRIVÉ - Clinique Médicale</h6>
                     <p>
                         3333 Blvd Graham #220 <br>Mont Royal, Québec H3R 3L5<br>
@@ -116,50 +116,10 @@
                         +1 (833) 921-2783
                     </p>
                   </div>
-                  <div class="col-lg-3">
-                    <h6>SM PRIVÉ - Clinique Médicale</h6>
-                    <p>
-                        3333 Blvd Graham #220 <br>Mont Royal, QC H3R 3L5<br>
-                        <a target="_blank" href="mailto:service@santemedic.com">service@santemedic.com</a><br>
-                        +1 (833) 921-2783
-                    </p>
-                  </div>
+                  
                 </div>  
 
-                <div class="row vr-size-1 clinic-info">
-                  <div class="col-lg-3">
-                    <h6>SM PRIVÉ - Clinique Médicale</h6>
-                    <p>
-                        3333 Blvd Graham #220 <br>Mont Royal, Québec H3R 3L5<br>
-                        <a target="_blank" href="mailto:service@santemedic.com">service@santemedic.com</a><br>
-                        +1 (833) 921-2783
-                    </p>
-                  </div>
-                  <div class="col-lg-3">
-                    <h6>SM PRIVÉ - Clinique Médicale</h6>
-                    <p>
-                        3333 Blvd Graham #220 <br>Mont Royal, Québec H3R 3L5<br>
-                        <a target="_blank" href="mailto:service@santemedic.com">service@santemedic.com</a><br>
-                        +1 (833) 921-2783
-                    </p>
-                  </div>
-                  <div class="col-lg-3">
-                    <h6>SM PRIVÉ - Clinique Médicale</h6>
-                    <p>
-                        3333 Blvd Graham #220 <br>Mont Royal, Québec H3R 3L5<br>
-                        <a target="_blank" href="mailto:service@santemedic.com">service@santemedic.com</a><br>
-                        +1 (833) 921-2783
-                    </p>
-                  </div>
-                  <div class="col-lg-3">
-                    <h6>SM PRIVÉ - Clinique Médicale</h6>
-                    <p>
-                        3333 Blvd Graham #220 <br>Mont Royal, Québec H3R 3L5<br>
-                        <a target="_blank" href="mailto:service@santemedic.com">service@santemedic.com</a><br>
-                        +1 (833) 921-2783
-                    </p>
-                  </div>
-                </div>
+                
               </div>
             </div>
 
